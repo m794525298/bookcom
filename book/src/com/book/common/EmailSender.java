608929b855d673fc7edcd407d57600ae40bfad9e
worprime <37793599+worprime@users.noreply.github.com>
2020-04-28 18:08:39 +0800

    Add files via upload

--- book/src/com/book/common/EmailSender.java
+++ book/src/com/book/common/EmailSender.java
@@ -1,11 +1,9 @@
 package com.book.common;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
--- book/src/com/book/service/Login.java
+++ book/src/com/book/service/Login.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,10 +27,28 @@ public class Login extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String account = new String(request.getParameter("account").getBytes("ISO-8859-1"),"UTF-8");
 		String password = new String(request.getParameter("password"));
-		if (login(account,password)) {
-			response.getWriter().append("登錄成功");
-		}else{
-			response.getWriter().append("用戶名或密碼錯誤");
+		ResultSet rs = login(account,password);
+		Cookie cookie;
+		try {
+			if (!rs.wasNull()) {
+				cookie = new Cookie("USER_ID",Coder.encryptedPassword(String.valueOf(rs.getInt("USER_ID"))));
+				cookie.setMaxAge(3600);
+				response.addCookie(cookie);
+				
+				cookie = new Cookie("USER_NICKNAME",rs.getString("USER_NICKNAME"));
+				cookie.setMaxAge(3600);
+				response.addCookie(cookie);
+				
+				cookie = new Cookie("USER_ICON",rs.getString("USER_ICON"));
+				cookie.setMaxAge(3600);
+				response.addCookie(cookie);
+				
+				response.getWriter().append("登錄成功");
+			}else{
+				response.getWriter().append("用戶名或密碼錯誤");
+			}
+		} catch (SQLException | NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
 		}
 	}
 
@@ -38,18 +57,15 @@ public class Login extends HttpServlet{
 		doGet(request, response);
 	}
 
-	public boolean login(String account, String password) {
+	public ResultSet login(String account, String password) {
 		UserDao userDao = new UserDao();
 		ResultSet rs;
 		try {
 			rs = userDao.login(account, Coder.encryptedPassword(password));
-			return (rs.wasNull())?false:true;
-		} catch (NoSuchAlgorithmException e1) {
-			return false;
-		} catch (SQLException e) {
-			return false;
+			return rs;
+		} catch (NoSuchAlgorithmException e) {
+			return null;
 		}
-		
 	}
 
 
