2551d804fb4e1699fe8c8b44f7798a17c3a35682
ITK37412 <43299501+ITK37412@users.noreply.github.com>
2020-05-18 21:31:47 +0800

    Add files via upload

---
+++ book/src/com/book/pojo/CommentBean.java
@@ -0,0 +1,10 @@
+package com.book.pojo;
+
+public class CommentBean {
+	private String id;
+	private String time;
+	private String parent;
+	private String postId;
+	private String content;
+	private String publisherId;
+}
--- book/src/com/book/pojo/PostBean.java
+++ book/src/com/book/pojo/PostBean.java
@@ -1,52 +1,120 @@
 package com.book.pojo;
 
 public class PostBean {
-	private String title;
-	private String publisherId;
-	private String content;
-	private String bookId;
+	private String id;
+	private String time;
 	private String cover;
-	PostBean(){
+	private String content;
+	private String postTitle;
+	private String bookType;
+	private String bookTitle;
+	private String publisherId;
+	private String bookAuthor;
+	private String commentNum;
+	private String isExist;
+	
+	public PostBean(String id, String time, String cover, String content, String postTitle, String bookType,
+			String bookTitle, String publisherId, String bookAuthor, String commentNum, String isExist) {
 		super();
-	}
-	
-	public void setCover(String cover) {
+		this.id = id;
+		this.time = time;
 		this.cover = cover;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public  void setPublisherId(String publisherId) {
-		this.publisherId = publisherId;
-	}
-	
-	public  void setContent(String content) {
 		this.content = content;
+		this.postTitle = postTitle;
+		this.bookType = bookType;
+		this.bookTitle = bookTitle;
+		this.publisherId = publisherId;
+		this.bookAuthor = bookAuthor;
+		this.commentNum = commentNum;
+		this.isExist = isExist;
 	}
-	
-	public  void setBookId(String bookId) {
-		this.bookId = bookId;
+
+	public String getId() {
+		return id;
 	}
-	
-	public  String getTitle() {
-		return title;
+
+	public void setId(String id) {
+		this.id = id;
 	}
-	
-	public  String getPublisherId() {
-		return publisherId;
+
+	public String getTime() {
+		return time;
 	}
-	
-	public  String getContent() {
-		return content;
+
+	public void setTime(String time) {
+		this.time = time;
 	}
-	
-	public  String getBookId() {
-		return bookId;
-	}
-	
+
 	public String getCover() {
 		return cover;
 	}
+
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getPostTitle() {
+		return postTitle;
+	}
+
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+
+	public String getBookType() {
+		return bookType;
+	}
+
+	public void setBookType(String bookType) {
+		this.bookType = bookType;
+	}
+
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+	public String getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(String publisherId) {
+		this.publisherId = publisherId;
+	}
+
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+
+	public String getCommentNum() {
+		return commentNum;
+	}
+
+	public void setCommentNum(String commentNum) {
+		this.commentNum = commentNum;
+	}
+
+	public String getIsExist() {
+		return isExist;
+	}
+
+	public void setIsExist(String isExist) {
+		this.isExist = isExist;
+	}
+	
 }
