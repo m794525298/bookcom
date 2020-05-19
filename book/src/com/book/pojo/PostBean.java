2f0f6bba1e03d2ca62f6dc3f165a5c55b3f1906f
ITK37412 <qwsa374293896@gmail.com>
2020-05-19 17:02:16 +0800

    update bean

--- book/src/com/book/pojo/PostBean.java
+++ book/src/com/book/pojo/PostBean.java
@@ -1,52 +1,121 @@
 package com.book.pojo;
 
 public class PostBean {
-	private String title;
-	private String publisherId;
-	private String content;
-	private String bookId;
+	private String id;
+	private String time;
 	private String cover;
+	private String content;
+	private String title;
+	private String bookType;
+	private String bookTitle;
+	private String publisherId;
+	private String bookAuthor;
+	private String commentNum;
+	private String isExist;
+	
 	PostBean(){
 		super();
 	}
 	
+	public PostBean(String id, String time, String cover, String content, String title, String bookType,
+			String bookTitle, String publisherId, String bookAuthor, String isExist) {
+		this.id = id;
+		this.time = time;
+		this.cover = cover;
+		this.content = content;
+		this.title = title;
+		this.bookType = bookType;
+		this.bookTitle = bookTitle;
+		this.publisherId = publisherId;
+		this.bookAuthor = bookAuthor;
+		this.isExist = isExist;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getCover() {
+		return cover;
+	}
+
 	public void setCover(String cover) {
 		this.cover = cover;
 	}
-	
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	public  void setPublisherId(String publisherId) {
-		this.publisherId = publisherId;
+
+	public String getBookType() {
+		return bookType;
 	}
-	
-	public  void setContent(String content) {
-		this.content = content;
+
+	public void setBookType(String bookType) {
+		this.bookType = bookType;
 	}
-	
-	public  void setBookId(String bookId) {
-		this.bookId = bookId;
+
+	public String getBookTitle() {
+		return bookTitle;
 	}
-	
-	public  String getTitle() {
-		return title;
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
 	}
-	
-	public  String getPublisherId() {
+
+	public String getPublisherId() {
 		return publisherId;
 	}
-	
-	public  String getContent() {
-		return content;
+
+	public void setPublisherId(String publisherId) {
+		this.publisherId = publisherId;
 	}
-	
-	public  String getBookId() {
-		return bookId;
+
+	public String getBookAuthor() {
+		return bookAuthor;
 	}
-	
-	public String getCover() {
-		return cover;
+
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+
+	public String getCommentNum() {
+		return commentNum;
+	}
+
+	public void setCommentNum(String commentNum) {
+		this.commentNum = commentNum;
+	}
+
+	public String getIsExist() {
+		return isExist;
+	}
+
+	public void setIsExist(String isExist) {
+		this.isExist = isExist;
 	}
 }
--- book/src/com/book/pojo/UserBean.java
+++ book/src/com/book/pojo/UserBean.java
@@ -2,32 +2,102 @@ package com.book.pojo;
 
 public class UserBean {
 	private String id;
-	private String icon;
 	private String nickname;
+	private String account;
+	private String password;
+	private String icon;
+	private String email;
+	private String identity;
+	private String followersNum;
+	private String followingNum;
+	
 	public UserBean(){
 		super();
 	}
-	public void setId(String id) {
+	
+	
+	public UserBean(String id, String nickname, String account, String password, String icon, String email, String identity) {
+		super();
 		this.id = id;
-	}
-	public void setIcon(String icon) {
-		this.icon = icon;
-	}
-	
-	public void setNickname(String nickname) {
 		this.nickname = nickname;
+		this.account = account;
+		this.password = password;
+		this.icon = icon;
+		this.email = email;
+		this.identity = identity;
 	}
-	
+
+
 	public String getId() {
 		return id;
 	}
-	
-	public String getIcon() {
-		return  icon;
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getNickname() {
 		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIdentity() {
+		return identity;
+	}
+
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+
+	public String getFollowersNum() {
+		return followersNum;
+	}
+
+	public void setFollowersNum(String followersNum) {
+		this.followersNum = followersNum;
+	}
+
+	public String getFollowingNum() {
+		return followingNum;
+	}
+
+	public void setFollowingNum(String followingNum) {
+		this.followingNum = followingNum;
 	}
 	
 
